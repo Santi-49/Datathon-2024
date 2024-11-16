c5ca523d08187aec6939bd3cbb3ba64748beefda
Santi R <romagosasantiago@gmail.com>
2024-11-16 12:04:32 +0100

    Read data

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Prueba
+import warnings
+import numpy as np
+import pandas as pd  # Install version 1.5.3 (iteritems errors)
+import os
+
+warnings.simplefilter(action='ignore', category=FutureWarning)  # Remove warning iteritems in Pool
+
+# read data
+print(os.listdir("./data"))
+train = pd.read_csv('./data/train.csv').iloc[:5000] # Seleccionar primeras 5000 columnas
+test = pd.read_csv('./data/test.csv')
 
