86a65b11ee1e29e65070ab2592f356694cc11978
Pablo <pablo.fernandez.aulet@estudiantat.upc.edu>
2024-11-17 09:41:18 +0100

    Refactor SHAP explanation function and improve visualization clarity

--- shap_module.py
+++ shap_module.py
@@ -4,7 +4,6 @@ import joblib
 import pandas as pd
 
 
-
 def explainer(model_catboost):
     return shap.TreeExplainer(model_catboost)
 
@@ -25,26 +24,36 @@ def shap_explain(explainer, X_test, sample_ind):
                                             data=X_test.iloc[sample_ind])
 
     # Gráfico de cascada para una instancia específica
-    shap.plots.waterfall(shap_values_instance, max_display=14)
+    plt.figure(figsize=(12, 8)) 
+    
+    # Ajustar la amplitud del eje X
+    plt.xlim(left=shap_values_instance.base_values - 1.5 * abs(shap_values_instance.values).max(),
+             right=shap_values_instance.base_values + 1.5 * abs(shap_values_instance.values).max())
+    
+    # Mejorar la claridad del texto en el eje Y
+    plt.yticks(fontsize=12)  # Ajusta el tamaño de la fuente del eje Y
+    plt.xticks(fontsize=12)  # Ajusta el tamaño de la fuente del eje X
+    plt.tight_layout()  # Ajusta el diseño para evitar solapamientos
+    shap.plots.waterfall(shap_values_instance, max_display=20)
+    
     return shap_values_test
-
-TARGET = 'Listing.Price.ClosePrice'
-ID = 'Listing.ListingId'
-
-model_catboost_uploaded = joblib.load('./data/model_catboost.sav')
-X_train = pd.read_csv('./data/X_train.csv')
-Y_train = pd.read_csv('./data/Y_train.csv')
-X_test = pd.read_csv('./data/X_test.csv')
-
-Categorical = pd.read_csv('./data/categorical.csv')['0'].drop(0)
-# 3) Transform categorical
-X_train[Categorical] = X_train[Categorical].fillna('nann').astype(str)
-X_test[Categorical] = X_test[Categorical].fillna('nann').astype(str)
 
 
 def main():
+    TARGET = 'Listing.Price.ClosePrice'
+    ID = 'Listing.ListingId'
+
+    model_catboost_uploaded = joblib.load('./data/model_catboost.sav')
+    X_train = pd.read_csv('./data/X_train.csv')
+    Y_train = pd.read_csv('./data/Y_train.csv')
+    X_test = pd.read_csv('./data/X_test.csv')
+
+    Categorical = pd.read_csv('./data/categorical.csv')['0'].drop(0)
+    # 3) Transform categorical
+    X_train[Categorical] = X_train[Categorical].fillna('nann').astype(str)
+    X_test[Categorical] = X_test[Categorical].fillna('nann').astype(str)
     exp = explainer(model_catboost_uploaded)
-    shap_values = shap_summary(exp, X_train)
+    #shap_values = shap_summary(exp, X_train)
     shap_values_test = shap_explain(exp, X_test, 100)
 
 if __name__ == '__main__':
